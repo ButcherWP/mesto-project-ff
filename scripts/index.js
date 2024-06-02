10901cb0e97dbd487422c59d690c1db6b8b20a07
Mikhail Slobodian <solomon131131131@yandex.ru>
2024-06-02 15:08:38 +0500

    commit-1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard (card, {deleteCard}) {
+    const cloneTemplate = cardTemplate.querySelector('.card').cloneNode(true);
+    cloneTemplate.querySelector('.card__title').textContent = card.name;
+    cloneTemplate.querySelector('.card__image').src = card.link;
+
+    const cardDeleteButton = cloneTemplate.querySelector('.card__delete-button');
+        cardDeleteButton.addEventListener('click', () => {
+            deleteCard(cloneTemplate);
+        });
+
+    return cloneTemplate;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(card) {
+    card.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (card) {
+    placesList.append(createCard(card, {deleteCard}))
+});
