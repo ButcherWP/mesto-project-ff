7fb505e46dafb52b4a9e5df801e4bbf952839d35
Mikhail Slobodian <solomon131131131@yandex.ru>
2024-06-03 11:25:37 +0500

    lastFix

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,15 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard (card, deleteCard) {
+function createCard (card, callback) {
     const cloneTemplate = cardTemplate.querySelector('.card').cloneNode(true);
     cloneTemplate.querySelector('.card__title').textContent = card.name;
     cloneTemplate.querySelector('.card__image').src = card.link;
+    cloneTemplate.querySelector('.card__image').alt = card.name;
 
     const cardDeleteButton = cloneTemplate.querySelector('.card__delete-button');
-        cardDeleteButton.addEventListener('click', () => {
-            deleteCard(cloneTemplate);
+    cardDeleteButton.addEventListener('click', () => {
+            callback(cloneTemplate);
         });
 
     return cloneTemplate;
